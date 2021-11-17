2d6fcb4eab4c0cc93c4ffd8d0e352ee72de9102d
Quazi Hasnat Irfan <80903120+Irfan-IIUC@users.noreply.github.com>
2021-11-17 22:31:59 +0600

    Add files via upload

---
+++ fibonacci.cpp
@@ -0,0 +1,20 @@
+/*
+    n = 4
+    number = 1, 2
+    {(1,1,1,1), (1,1,2), (1,2,1), (2,1,1), (2,2)} = 5 ways
+*/
+
+#include<stdio.h>
+int main()
+{
+    int way[50], n=4, i;
+    way[0] = way[1] = 1;
+
+    for(i=2; i<=n; i++)
+    {
+        way[i] = way[i-1] + way[i-2];
+    }
+
+    printf("%d\n", way[n]);
+    return 0;
+}
