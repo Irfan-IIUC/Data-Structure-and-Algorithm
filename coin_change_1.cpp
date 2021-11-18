c19f0e39e5c2879be7c4ec0eb963755ed1b62462
Quazi Hasnat Irfan <80903120+Irfan-IIUC@users.noreply.github.com>
2021-11-18 10:58:49 +0600

    Add files via upload

---
+++ coin_change_1.cpp
@@ -0,0 +1,33 @@
+/*
+    coin = 4, 7, 15 [total coin, k = 3]
+    n = 15 [can you make 15]
+    {4, 4, 7} = 15
+*/
+
+#include<stdio.h>
+int main()
+{
+    int possible[100]={0}, coin[3]={4,7,15}, k=3, n=15, i, j;
+    possible[0] = 1;
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            if(i>=coin[j])
+            {
+                possible[i] |= possible[i-coin[j]];
+            }
+        }
+    }
+
+    for(i=0; i<=n; i++)
+        printf("%d ", possible[i]);
+
+    if(possible[n] == 1)
+        printf("\nYes\n");
+    else
+        printf("\nNo\n");
+
+    return 0;
+}
---
+++ coin_change_2.cpp
@@ -0,0 +1,29 @@
+/*
+    coin = 1, 2 [total coin, k=2]
+    n = 4 [you have to make 4]
+    {(1,1,1,1), (1,1,2), (1,2,1), (2,1,1), (2,2)} = 5 ways
+*/
+
+#include<stdio.h>
+int main()
+{
+    int way[100]={0}, coin[2]={1, 2}, k=2, n=4, i, j;
+    way[0] = 1;
+
+    for(i=1; i<=n; i++)
+    {
+        for(j=0; j<k; j++)
+        {
+            if(i>=coin[j])
+            {
+                way[i] += way[i-coin[j]];
+            }
+        }
+    }
+
+    for(i=0; i<=n; i++)
+        printf("%d ", way[i]);
+
+    printf("\n%d\n", way[n]);
+    return 0;
+}
